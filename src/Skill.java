3fed4daf50811764a7a1b62d6856eb77f22d90af
ww <vcdbrovcd@ww.com>
2022-05-14 20:32:50 +0800

    skill

---
+++ src/Skill.java
@@ -0,0 +1,23 @@
+public class Skill {
+    //等概率返回1——>5
+    public static int f(){
+        return (int) (Math.random() * 5) + 1;
+    }
+    //将f()修改成等概率返回0或1
+    public static int r01(){
+        int res = 0;
+        do {
+            res = f();
+        }while (res == 3);
+        return res < 3 ? 0 : 1;
+    }
+    //等概率返回1——>7
+    public static int g(){
+        int res = 0;
+        do {
+            res = (r01() << 2) + (r01() << 1) + r01();
+        }while (res == 7);
+        return res + 1;
+    }
+    //===========以上将等概率返回1——>5修改成等概率返回1——>7=========
+}
