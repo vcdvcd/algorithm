c6a3a2fc0d864127fd86965ff76964063dbbe41e
ww <vcdbrovcd@ww.com>
2022-05-16 20:59:01 +0800

    skill3

--- src/Skill.java
+++ src/Skill.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 public class Skill {
     //等概率返回1——>5
@@ -38,6 +40,65 @@ public class Skill {
             max = Math.max(max,dp[i]);
         }
         return max;
+    }
+    //二维数组查找数字
+    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+        int n = matrix.length;
+        if(n == 0) return false;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m - 1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] > target){
+                col--;
+            }
+            else if(matrix[row][col] < target){
+                row++;
+            }
+            else return true;
+        }
+        return false;
+    }
+    //螺旋输出矩阵
+    public static List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int a = 0;
+        int b = 0;
+        int c = n - 1;
+        int d = m - 1;
+        while(a <= c && b <= d){
+            if(a == c){
+                for(int i = b;i <= d;i++){
+                    ans.add(matrix[a][i]);
+                }
+            }else if(b == d){
+                for(int i = a;i <= c;i++){
+                    ans.add(matrix[i][b]);
+                }
+            }else{
+                int a1 = a;
+                int b1 = b;
+                while(b1 != d){
+                    ans.add(matrix[a][b1++]);
+                }
+                while(a1 != c){
+                    ans.add(matrix[a1++][d]);
+                }
+                while(b1 != b){
+                    ans.add(matrix[c][b1--]);
+                }
+                while(a1 != a){
+                    ans.add(matrix[a1--][b]);
+                }
+            }
+            a++;
+            b++;
+            c--;
+            d--;
+        }
+        return ans;
     }
     public static void main(String[] args) {
         HashMap<Integer,Integer> map = new HashMap<>();
