848365905c4e36aa4f5344bbd3f63d2b29240a9f
ww <vcdbrovcd@ww.com>
2022-05-17 20:42:22 +0800

    skill4

--- src/Skill.java
+++ src/Skill.java
@@ -191,12 +191,12 @@ public class Skill {
         int[][] m1 = {{0,1,2,3},{4,5,6,7},{8,9,10,11}};
         rotate(m);
         zigzag(m1);
-//        for (int i = 0; i < m.length; i++) {
-//            for (int j = 0; j < m[0].length; j++) {
-//                System.out.print(m[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
+        for (int i = 0; i < m.length; i++) {
+            for (int j = 0; j < m[0].length; j++) {
+                System.out.print(m[i][j] + " ");
+            }
+            System.out.println();
+        }
 
     }
 }
